278111ff8a5bc4186d8da7bbde879a94bbe42aa1
hunzzzzz <152062846+hunzzzzz@users.noreply.github.com>
2024-03-14 16:47:08 +0900

    [Feat] 예외 처리를 위한 ErrorResponse, ExceptionHandler 클래스 생성
    - ErrorResponse 클래스 생성 (에러 내용을 json 형식으로 리턴)
    - ExceptionHandler 클래스 생성 (전역적인 에러 처리를 담당)
    - ZonedDateTime 을 String의 날짜+시간 데이터로 변환하기 위한 ZonedDateTimeConverter Object 생성
        - ZonedDateTime → String
        - String → ZonedDate

---
+++ src/main/kotlin/com/jo/mata/global/exception/ErrorResponse.kt
@@ -0,0 +1,11 @@
+package com.jo.mata.global.exception
+
+import com.jo.mata.global.utiliy.ZonedDateTimeConverter
+import java.time.ZonedDateTime
+
+data class ErrorResponse(
+    val httpStatus: String,
+    val message: String,
+    val path: String,
+    val time: String = ZonedDateTimeConverter.convertZonedDateTimeIntoStringDateTime(ZonedDateTime.now())
+)
